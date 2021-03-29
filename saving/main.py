5f5ce780f51e6bcd67bcc27fe2ad4371f6dbafc2
bagas-I56 <banabda@gmail.com>
2021-03-30 06:46:22 +0700

    seeder

--- saving/main.py
+++ saving/main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
 from . import models
-from .database import engine
+from .database import engine, get_db, SessionLocal
 from .routers import auth, saving, user
-
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from sqlalchemyseeder.basic_seeder import BasicSeeder
+from sqlalchemyseeder.resolving_seeder import ResolvingSeeder
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
@@ -11,7 +13,19 @@ app.include_router(auth.router)
 app.include_router(saving.router)
 app.include_router(user.router)
 
+# session = SessionLocal()
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get('/seed')
+def show(db: Session = Depends(get_db)):
+    seeder = ResolvingSeeder(session=db)
+    seeder.registry.register_class(models.Role)
+    seeder.load_entities_from_json_file(
+        "saving\\seeder\\role.json")
+    db.commit()
+    return 'ok'
--- saving/models.py
+++ saving/models.py
@@ -1,18 +1,16 @@
 from .database import Base
-from sqlalchemy import Column, String, Integer, DateTime, UniqueConstraint, ForeignKey
+from sqlalchemy import Column, String, Integer, DateTime, UniqueConstraint, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 from datetime import datetime
 
 
-class Saving(Base):
-    __tablename__ = 'savings'
+class Role(Base):
+    __tablename__ = 'roles'
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    body = Column(String)
-    user_id = Column(Integer, ForeignKey('users.id'))
+    name = Column(String)
     created_at = Column(DateTime, default=datetime.now())
     updated_at = Column(DateTime, default=None)
-    creator = relationship('User', back_populates='savings')
+    the_user = relationship('User', back_populates='the_role')
 
 
 class User(Base):
@@ -20,7 +18,48 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     email = Column(String, unique=True)
+    avatar = Column(String)
     password = Column(String)
+    phone = Column(String)
+    token = Column(String)
+    verified = Column(Boolean)
+    role = Column(Integer, ForeignKey(Role.id))
     created_at = Column(DateTime, default=datetime.now())
     updated_at = Column(DateTime, default=None)
-    savings = relationship('Saving', back_populates='creator')
+    savings = relationship('Transaction', back_populates='creator')
+    the_role = relationship('Role', back_populates='the_user')
+    has_role = relationship('UserHasRole', back_populates='the_user_has_role')
+
+
+class UserHasRole(Base):
+    __tablename__ = 'user_has_roles'
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey(User.id))
+    created_at = Column(DateTime, default=datetime.now())
+    updated_at = Column(DateTime, default=None)
+    the_user_has_role = relationship('User', back_populates='has_role')
+
+
+class Balance(Base):
+    __tablename__ = 'balances'
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey(User.id))
+    amount = Column(Integer)
+    description = Column(String)
+    created_at = Column(DateTime, default=datetime.now())
+    updated_at = Column(DateTime, default=None)
+    transac = relationship('Transaction', back_populates='balan')
+
+
+class Transaction(Base):
+    __tablename__ = 'transactions'
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey(User.id))
+    balance_id = Column(Integer, ForeignKey(Balance.id))
+    amount = Column(Integer)
+    type = Column(String)
+    description = Column(String)
+    created_at = Column(DateTime, default=datetime.now())
+    updated_at = Column(DateTime, default=None)
+    creator = relationship('User', back_populates='savings')
+    balan = relationship('Balance', back_populates='transac')
